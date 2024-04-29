cf36ddf7a3e3ec757b1e592026733d0911b9c6a6
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-04-29 12:58:19 -0700

    hasher

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -26,4 +27,11 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        if kwargs:
+            pw = kwargs.pop('pasword', None)
+            if pw:
+                hashe = hashlib.md5()
+                hashe.update(pw.encode('utf-8'))
+                secure = hashe.hexdigest()
+                kwargs['password'] = secure
         super().__init__(*args, **kwargs)
